b89727f17cb346e144f6d682ab4ecd02da621034
afilonov <filon13p@gmail.com>
2021-09-18 12:56:16 +0300

    закончил ЛР2

--- LR2/src/main/java/crypto/WinstonCryptoEncoder.java
+++ LR2/src/main/java/crypto/WinstonCryptoEncoder.java
@@ -34,24 +34,46 @@ public class WinstonCryptoEncoder implements CryptoEncoder {
 
     }
 
+
+    @Override
+    public String decrypt(String data) {
+        String decrypted_data = Arrays.stream(data.replaceAll("..(?!$)", "$0 ")
+                .split(" "))
+                .map(this::decryptBigram)
+                .reduce((s, s2) -> s + s2).get();
+
+
+        if (decrypted_data.charAt(decrypted_data.length() - 1) == 'ъ')
+            decrypted_data = decrypted_data.substring(0, decrypted_data.length() - 1);
+        return decrypted_data;
+    }
+
+
     public String encryptBigram(String bigram) {
         Pair<Integer, Integer> leftPosition = findChar(bigram.charAt(0), table1);
-
-        Pair<Integer, Integer> rightPosition = findChar(bigram.length() == 2 ? bigram.charAt(1) : 'а', table2);
-        Character leftEncryptedChar;
-        Character rightEncryptedChar;
+        Pair<Integer, Integer> rightPosition = findChar(bigram.length() == 2 ? bigram.charAt(1) : 'ъ', table2);
 
 
-        if (leftPosition.getKey() <= rightPosition.getKey()) {
-            leftEncryptedChar = table2[leftPosition.getKey()][rightPosition.getValue()];
-            rightEncryptedChar = table1[rightPosition.getKey()][leftPosition.getValue()];
-        } else {
-            leftEncryptedChar = table1[rightPosition.getKey()][leftPosition.getValue()];
-            rightEncryptedChar = table2[leftPosition.getKey()][rightPosition.getValue()];
-        }
+        Character leftEncryptedChar = table2[leftPosition.getKey()][rightPosition.getValue()];
+        Character rightEncryptedChar = table1[rightPosition.getKey()][leftPosition.getValue()];
+
 
         String encryptedBigram = leftEncryptedChar + String.valueOf(rightEncryptedChar);
         System.out.println("Encrypted '" + bigram + "' to '" + encryptedBigram + "'");
+
+        return encryptedBigram;
+    }
+
+    public String decryptBigram(String bigram) {
+        Pair<Integer, Integer> leftPosition = findChar(bigram.charAt(0), table2);
+        Pair<Integer, Integer> rightPosition = findChar(bigram.charAt(1), table1);
+
+
+        Character leftDecryptedChar = table1[leftPosition.getKey()][rightPosition.getValue()];
+        Character rightDecryptedChar = table2[rightPosition.getKey()][leftPosition.getValue()];
+
+        String encryptedBigram = leftDecryptedChar + String.valueOf(rightDecryptedChar);
+        System.out.println("Decrypted '" + bigram + "' to '" + encryptedBigram + "'");
 
         return encryptedBigram;
     }
@@ -66,8 +88,4 @@ public class WinstonCryptoEncoder implements CryptoEncoder {
         throw new RuntimeException("Char " + c + " not found");
     }
 
-    @Override
-    public String decrypt(String data) {
-        return null;
-    }
 }
