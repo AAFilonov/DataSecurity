b0095f1b3a8e923c9f9dfbb164389d6b6aab5f44
afilonov <filon13p@gmail.com>
2021-12-25 16:25:19 +0300

    логирование

--- Kursach/src/main/java/com/kursach/kursach/service/HashService.java
+++ Kursach/src/main/java/com/kursach/kursach/service/HashService.java
@@ -1,6 +1,8 @@
 package com.kursach.kursach.service;
 
 import com.kursach.hash.GostHashJava;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
@@ -14,20 +16,22 @@ import static com.kursach.utils.StringToByteConverter.bytesToHex;
 @Service
 public class HashService {
     private static GostHashJava javaHash = new GostHashJava();
-
+    private final static Logger log = LoggerFactory.getLogger(HashService.class);
 
     public String calcHash(String data) {
+        log.info("Data to hash: {}", data);
         var data_bytes = data.getBytes(StandardCharsets.UTF_8);
         javaHash.init();
         var bytes = javaHash.calcHash(new ByteArrayInputStream(data_bytes));
         javaHash.finishHash();
+        log.info("Hashing result: {}", bytesToHex(bytes));
         return bytesToHex(bytes);
     }
 
     public static byte[] calcHash(File file) {
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
             javaHash.init();
-            var output =  javaHash.calcHash(fileInputStream);
+            var output = javaHash.calcHash(fileInputStream);
             javaHash.finishHash();
             return output;
         } catch (IOException x) {
